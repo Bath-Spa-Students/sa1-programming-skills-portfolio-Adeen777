398ea918c2b7745405ac1e5e707116e81b6326af
FAAZ <faaz07009@gmail.com>
2024-10-16 14:53:43 +0400

    Create codingiscool.py
    Coding is cool

---
+++ exersice 01/codingiscool.py
@@ -0,0 +1,8 @@
+#Giving the first word to the variable word1
+word1 = "Coding"
+#Giving the first word to the variable word2
+word2 = "is"
+#Giving the first word to the variable word3
+word3 = "Cool"
+result = word1+" "+word2+" "+word3
+print(result)
