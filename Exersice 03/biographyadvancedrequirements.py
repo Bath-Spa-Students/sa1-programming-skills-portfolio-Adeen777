7e7c87ae5578bb6a27c6d78eb15d228b5acea334
FAAZ <faaz07009@gmail.com>
2024-11-21 09:47:54 +0400

    biographyadvancedrequirements.py

--- Exersice 03/biographyadvancedrequirements.py
+++ Exersice 03/biographyadvancedrequirements.py
@@ -1,13 +1,15 @@
 # Creating a dictionary to store information 
 my_info = {"name": input("enter your name (only enter your first and second name): "),"hometown": input("enter your hometown: ")}
 
+# taking age outside the dictionary as age cannot be entered in string 
 my_age = input("Please enter your age: ")
 
+# cheaking if the age is a string or an integer
 while not my_age.isdigit():
-    print("Invalid input! Please enter a valid age (numeric value).")
+    print("your age must be entered in number!")
     my_age = input("Please enter your age: ")
 
-# Convert the age to an integer
+# Converting age to integer
 my_info["age"] = int(my_age)
 
 # Printing information
