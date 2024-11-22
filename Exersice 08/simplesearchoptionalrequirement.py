402e205910766bb0ea82c6fc5e35ca9eca20006e
FAAZ <faaz07009@gmail.com>
2024-11-22 15:38:20 +0400

    simplesearchoptionalrequirement.py
    exercise 8

--- Exersice 08/simplesearchoptionalrequirement.py
+++ Exersice 08/simplesearchoptionalrequirement.py
@@ -1,17 +1,11 @@
-def find_name(name_list, search_name):
-    if search_name in name_list:
-        return f"{search_name} is found in list."
-    else:
-        return f"{search_name} is not found in list."
+# Initial the list of names
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
-def main():
-#list of names
-    names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
- #asking user to enter the name they are searching for
-    search_term = input("Enter the name you want to search for: ")
-#Call the find_name function 
-    result = find_name(names, search_term)
-    print(result)
+# asking user name we're searching for
+search_name = input("ENTER THE NAME YOU ARE LOOKING FOR: ")
 
-if __name__ == "__main__":
-    main()
+# Check if the given name is in the list
+if search_name in names:
+    print(f"{search_name} was found ")
+else:
+    print(f"{search_name} was not found ")
