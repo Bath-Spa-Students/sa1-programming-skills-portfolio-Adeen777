765c39a7c1c196145af949a760d7f7fe5ce07d05
FAAZ <faaz07009@gmail.com>
2024-11-22 14:33:23 +0400

    hello.py
    exercise 9

---
+++ Exersice 09/hello.py
@@ -0,0 +1,11 @@
+def hello():
+    print("Hello") 
+
+def main():
+    hello()  
+# this is to Displays "Hello" on to the console.
+
+# This line checks if script is being run directly or indirectly.
+if __name__ == "__main__":
+    main()  
+# This calls main function.
